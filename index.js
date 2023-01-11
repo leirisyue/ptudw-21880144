6bbacebda6e40c9687b75748be4b395da6d1c5f0
lntt.thuy <lntt.thuy@gmail.com>
2023-01-11 16:40:12 +0700

    create new project

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strick';
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/',(req,res)=>{
+   res.send('Hello to EShop')
+})
+app.listen(port,()=>{
+   console.log(`Server is running on port ${port}`)
+})
