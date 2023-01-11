a15cce6469e41575d33c59f2fbe0a2373dd7410e
lntt.thuy <lntt.thuy@gmail.com>
2023-01-11 16:43:31 +0700

    up

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
-   res.send('Hello to EShop')
-})
+app.use(express.static(__dirname + '/public'));
+// app.get('/',(req,res)=>{
+//    res.send('Hello to EShop')
+// })
 app.listen(port,()=>{
    console.log(`Server is running on port ${port}`)
 })
